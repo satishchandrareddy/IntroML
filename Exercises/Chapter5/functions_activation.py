ceeeade087731d800ef479e19c2b82fa1d693759
kristokrugger <kristoelijahkrugger@gmail.com>
2020-09-03 14:58:31 -0700

    chapter 4,5 programming exercises completed

---
+++ Exercises/Chapter5/functions_activation.py
@@ -0,0 +1,40 @@
+# functions_activation.py
+
+import numpy as np
+
+def activation(activation_fun,Z):
+	LIM = 50
+	if activation_fun == "linear":
+		return Z
+	elif activation_fun == "sigmoid":
+		ZLIM = np.maximum(Z,-LIM)
+		return 1/(1+np.exp(-ZLIM))
+	elif activation_fun == "mirrorsigmoid":
+		ZLIM = np.maximum(Z,LIM)
+		return 1/(1+np.exp(ZLIM))
+	elif activation_fun == "relu":
+		return np.maximum(Z,0.0)
+	elif activation_fun == "tanh":
+		return np.tanh(Z)
+	elif activation_fun == "softplus":
+		ZLIM = np.maximum(Z,-LIM)
+		return ZLIM + np.log(np.exp(-ZLIM)+1)
+	elif activation_fun == "elu":
+		ZLIM = np.maximum(Z,LIM)
+		return Z*np.heaviside(Z, 1) + (np.exp(ZLIM)-1)*np.heaviside(-Z, 0)
+
+def activation_der(activation_fun,A,grad_A_L):
+	if activation_fun == "linear":
+		return grad_A_L*np.ones(A.shape)
+	elif activation_fun == "sigmoid":
+		return grad_A_L*(A - np.square(A))
+	elif activation_fun == "mirrorsigmoid":
+		return grad_A_L*(np.square(A) - A)
+	elif activation_fun == "relu":
+		return grad_A_L*(np.piecewise(A,[A<0,A>=0],[0,1]))
+	elif activation_fun == "tanh":
+		return grad_A_L*(1 - np.square(A))
+	elif activation_fun == "softplus":
+		return grad_A_L*(1 - np.exp(-A))
+	elif activation_fun == "elu":
+		return grad_A_L*(np.heaviside(A, 1) + (A+1)*np.heaviside(-A, 0))
---
+++ Exercises/Chapter5/unittest_forwardbackprop_elu.py
@@ -0,0 +1,124 @@
+#unittest_forwardbackprop.py
+
+import LRegression
+import NeuralNetwork
+import numpy as np
+import unittest
+
+class Test_functions(unittest.TestCase):
+    
+    def test_LinearRegression(self):
+        # (1) create input/output training data X and Y (random)
+        nfeature = 8
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.random.rand(1,m)
+        # (2) define object
+        model = LRegression.LRegression(nfeature,"linear")
+        # (3) compile
+        optimizer = None
+        model.compile("meansquarederror",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: LinearRegression Error: {}".format(error))
+        # (5) assert statement
+        self.assertLessEqual(error,1e-7)
+
+    def test_LinearRegression_logcosh(self):
+        # (1) create input/output training data X and Y (random)
+        nfeature = 8
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.random.rand(1,m)
+        # (2) define object
+        model = LRegression.LRegression(nfeature,"linear")
+        # (3) compile
+        optimizer = None
+        model.compile("logcosh",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: LinearRegression Error: {}".format(error))
+        # (5) assert statement
+        self.assertLessEqual(error,1e-7)
+  
+    def test_LogisticRegression(self):
+        # (1) create input/output training data X and Y (random)
+        nfeature = 2
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.round(np.random.rand(1,m))
+        # (2) define object
+        model = LRegression.LRegression(nfeature,"sigmoid")
+        # (3) compile
+        optimizer = None
+        model.compile("binarycrossentropy",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: LogisticRegression Error: {}".format(error))
+        self.assertLessEqual(error,1e-7)
+
+    def test_LogisticRegression_mirrorsigmoid(self):
+        # (1) create input/output training data X and Y (random)
+        nfeature = 2
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.round(np.random.rand(1,m))
+        # (2) define object
+        model = LRegression.LRegression(nfeature,"mirrorsigmoid")
+        # (3) compile
+        optimizer = None
+        model.compile("binarycrossentropy",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: LogisticRegression Error: {}".format(error))
+        # assert statement
+        self.assertLessEqual(error,1e-7)
+
+    def test_NeuralNetwork_binary(self):
+        # (1) create input/output training data X and Y
+        nfeature = 2
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.round(np.random.rand(1,m))
+        # (2) define neural network
+        model = NeuralNetwork.NeuralNetwork(nfeature)
+        model.add_layer(5,"softplus")
+        model.add_layer(3,"tanh")
+        model.add_layer(1,"sigmoid")
+        # (3) compile
+        optimizer = None
+        model.compile("binarycrossentropy",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: 3 layer NeuralNetwork Error: {}".format(error))
+        # (5) assert
+        self.assertLessEqual(error,1e-7)
+
+    def test_NeuralNetwork_binary_elu(self):
+        # (1) create input/output training data X and Y
+        nfeature = 2
+        m = 1000
+        X = np.random.rand(nfeature,m)
+        Y = np.round(np.random.rand(1,m))
+        # (2) define neural network
+        model = NeuralNetwork.NeuralNetwork(nfeature)
+        model.add_layer(5,"softplus")
+        model.add_layer(3,"elu")
+        model.add_layer(1,"sigmoid")
+        # (3) compile
+        optimizer = None
+        model.compile("binarycrossentropy",optimizer)
+        # (4) perform derivative test
+        eps = 1e-5
+        error = model.test_derivative(X,Y,eps)
+        print("forwardbackprop: 3 layer NeuralNetwork Error: {}".format(error))
+        # (5) assert
+        self.assertLessEqual(error,1e-7)
+
+if __name__ == "__main__":
+    unittest.main()
